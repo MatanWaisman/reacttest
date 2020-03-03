9de87691fe8e4f2558427e507514c614324920f5
MatanWaisman <matan.waisman@gmail.com>
2020-03-03 20:59:16 +0200

    Updates

---
+++ precache-manifest.6a27e11a32173be170398cae215989d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1dfabc6d15db02a3ac89c5795bee512",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "8c0d2f850eace6d4deb4",
+    "url": "/reacttest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7f0c105a0d18868e4780",
+    "url": "/reacttest/static/js/2.bc38623b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reacttest/static/js/2.bc38623b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c0d2f850eace6d4deb4",
+    "url": "/reacttest/static/js/main.d81a8aea.chunk.js"
+  },
+  {
+    "revision": "1cfbd8dd51c7c0ff4b92",
+    "url": "/reacttest/static/js/runtime-main.9c840396.js"
+  }
+]);
